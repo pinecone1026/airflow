8a89964ac84ec2f21e71c5209651312e39c1471b
joon0 <pinecone1026@gmail.com>
2024-06-25 14:50:29 +0900

    python_show_template dag modify

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_python_show_template",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2024, 6, 15, tz="Asia/Seoul"),
-    catchup=False
+    catchup=True
 ) as dag:
     
     @task(task_id='python_task')
