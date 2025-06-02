bff0bd3ad61ccde868152cd896e7155f7f228ce5
pinecone1026 <pinecone1026@gmail.com>
2025-06-03 07:43:33 +0900

    dags_bash_operator commit

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,33 @@
+import pendulum
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["airflow_study"]
+) as dag:
+    bash_t1 = BashOperator(
+        task_id = "bash_t1",
+        bash_command="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME; echo good",
+    )
+
+    bash_t3 = BashOperator(
+        task_id="bash_t3",
+        bash_command="echo $HOSTNAME; echo good",
+    )
+
+    bash_t4 = BashOperator(
+        task_id="bash_t4",
+        bash_command="echo $HOSTNAME; echo good",
+    )
+
+    bash_t1 >> bash_t2 >> bash_t3 >> bash_t4
