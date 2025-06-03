52103718751e7d8c755bce294fd2d6f04371af08
pinecone1026 <pinecone1026@gmail.com>
2025-06-04 07:37:38 +0900

    dags_bash_select_fruit 수정

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -11,8 +11,7 @@ with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False,
-    tags=["airflow_study"]
+    catchup=False
 ) as dag:
     
     t1_orange = BashOperator(
