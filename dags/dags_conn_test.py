476ef60d8f1cba8a46894cdd27bbeef9eab76427
pinecone1026 <pinecone1026@gmail.com>
2025-06-03 09:24:26 +0900

    tag 설정

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -11,7 +11,8 @@ with DAG(
     dag_id="dags_conn_test",
     schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False
+    catchup=False,
+    tags=["airflow_study"]
 ) as dag:
     
     t1 = EmptyOperator(
