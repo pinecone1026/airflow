86a59a91f3071c45382372edd6a934ba99a7055c
pinecone1026 <pinecone1026@gmail.com>
2025-06-26 08:19:26 +0900

    일단 commit

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -11,8 +11,7 @@ with DAG(
     dag_id="dags_conn_test",
     schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False,
-    tags=["airflow_study"]
+    catchup=False
 ) as dag:
     
     t1 = EmptyOperator(
